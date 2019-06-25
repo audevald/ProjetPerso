4f98a27b9ab1e3a71280326a165b99ea38de7e8a
audevald <51904673+audevald@users.noreply.github.com>
2019-06-25 14:54:54 +0200

    Add files via upload

--- formulaire.php
+++ formulaire.php
@@ -15,23 +15,56 @@ if (filter_input(INPUT_POST, 'submit')) {
     $client->nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
     $client->prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
     $client->tel = filter_input(INPUT_POST, 'tel', FILTER_VALIDATE_REGEXP, $regexp);
-    if ($reservation->heure > date('14:45') || $reservation->heure < date('19:00')) {
-        $tabErreur[] = "Impossible de réserver entre 15h00 et 19h00.";
+
+    // Verification Date
+    if (!$reservation->date_resa || $reservation->date_resa < date("Y-m-d")) {
+        $tabErreur[] = "La date est absente ou invalide.";
+    } else {
+        $_SESSION['date_resa'] = $reservation->date_resa;
     }
-    
+    // Vérification Heure
+    if ($reservation->heure > date('15:00') && $reservation->heure < date('19:00')) {
+        $tabErreur[] = "Impossible de réserver entre 15h00 et 19h00";
+    } else {
+        $_SESSION['heure'] = $reservation->heure;
+    }
+    // Vérification Nb_personne
+    if (!$reservation->nb_personne) {
+        $tabErreur[] = "Le nombre de couverts doit être renseigné.";
+    } else {
+            $_SESSION['nb_personne'] = $reservation->nb_personne;
+    }
+    // vérification Info
+    if (mb_strlen($reservation->info) > 150 ) {
+        $tabErreur[] = "Les informations complémentaires ne peuvent dépasser 150 carractères.";
+    } else {
+            $_SESSION['info'] = $reservation->info;
+    }
+    // Verification Nom
     if (!$client->nom || mb_strlen($client->nom) > 50) {
         $tabErreur[] = "Le Nom est absent ou invalide.";
+    } else {
+        $_SESSION['nom'] = $client->nom;
     }
-    if (!$client->tel || mb_strlen($client->tel) > 10) {
-        $tabErreur[] = "Le numéro de téléphone est absent ou incorrect.";
-    }
+    // Verification Prénom
     if (mb_strlen($client->prenom) > 30) {
         $tabErreur[] = "Le prenom est invalide.";
+    } else {
+        $_SESSION['prenom'] = $client->prenom;
     }
+    // Verification Tel
+    if (!$client->tel || mb_strlen($client->tel) > 10) {
+        $tabErreur[] = "Le numéro de téléphone est absent ou incorrect.";
+    } else {
+        $_SESSION['tel'] = $client->tel;
+    }
+
     if (!$tabErreur) {
         $client->sauver();
         $reservation->id_client = $client->id_client;
         $reservation->sauver();
+        $_SESSION = array();
+        $_SESSION['id_reservation'] = $reservation->id_reservation;
         header("Location:success.php");
         exit;
     }
@@ -66,70 +99,48 @@ if (filter_input(INPUT_POST, 'submit')) {
         <div class="container">
             <div class="row">
                 <div class="col-12 offset-sm-2 col-sm-8 offset-md-4 col-md-4">
-                    <form name="form1" action="formulaire.php" method="post">
-
+                    <form name="form1" action="formulaire.php" method="post"> 
                         <div class="form-group text-center">
                             <label for="champDate">Date du repas*</label>
-                            <input name="date_resa" type="date" class="form-control" id="champDate" min="<?= date("Y-m-d");?>" required />
+                            <input name="date_resa" type="date" value="<?= isset($_SESSION['date_resa']) ? $_SESSION['date_resa'] : ''; ?>" class="form-control" id="champDate" min="<?= date("Y-m-d"); ?>" required />
                         </div>
                         <div class="form-group text-center">
                             <label for="champHeure">Heure*</label>
-                            <input name="heure" type="time" class="form-control" id="champHeure" min="<?= date("12:00") ?>" max="<?= date("22:00") ?>" required />
+                            <input name="heure" type="time" class="form-control" value="<?= isset($_SESSION['heure']) ? $_SESSION['heure'] : ''; ?>" id="champHeure" min="<?= date("12:00") ?>" max="<?= date("22:00") ?>" required />
                         </div>
                         <div class="form-group text-center">
                             <label for="champNbr">Nombre de couverts*</label>
                             <select name="nb_personne" class="form-control nbr" id="champNbr" required>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                                <option>6</option>
-                                <option>7</option>
-                                <option>8</option>
-                                <option>9</option>
-                                <option>10</option>
-                                <option>11</option>
-                                <option>12</option>
-                                <option>13</option>
-                                <option>14</option>
-                                <option>15</option>
-                                <option>16</option>
-                                <option>17</option>
-                                <option>18</option>
-                                <option>19</option>
-                                <option>20</option>
-                                <option>21</option>
-                                <option>22</option>
-                                <option>23</option>
-                                <option>23</option>
-                                <option>24</option>
-                                <option>25</option>
-                                <option>26</option>
-                                <option>27</option>
-                                <option>28</option>
-                                <option>29</option>
-                                <option>30</option>
+                                <?php
+                                for ($i = 1; $i < 51; $i++) {
+                                    ?>
+                                    <option value="<?= $i ?>" <?= isset($_SESSION['nb_personne']) && $_SESSION['nb_personne'] == $i ? 'selected="selected"' : ''; ?>><?= $i ?></option>
+                                    <?php
+                                }
+                                ?>
+
+
                             </select>
                         </div>
-                        <div class="form-group text-center">
-                            <label for="champNom">Nom*</label>
-                            <input name="nom" type="text" maxlength="50" class="form-control" id="champNom" required />
+                        <div class = "form-group text-center">
+                            <label for = "champNom">Nom*</label>
+                            <input name = "nom" type = "text" value = "<?= isset($_SESSION['nom']) ? $_SESSION['nom'] : ''; ?>" maxlength = "50" class = "form-control" id = "champNom" required />
                         </div>
-                        <div class="form-group text-center">
-                            <label for="champPrenom">Prénom</label>
-                            <input name="prenom" type="text" maxlength="30" class="form-control" id="champPrenom" />
+                        <div class = "form-group text-center">
+                            <label for = "champPrenom">Prénom</label>
+                            <input name = "prenom" type = "text" value = "<?= isset($_SESSION['prenom']) ? $_SESSION['prenom'] : ''; ?>" maxlength = "30" class = "form-control" id = "champPrenom" />
                         </div>
-                        <div class="form-group text-center">
-                            <label for="champTel">Téléphone*</label>
-                            <input name="tel" type="tel" pattern="[0-9]{10}" maxlength="10" class="form-control" id="champTel" required />
+                        <div class = "form-group text-center">
+                            <label for = "champTel">Téléphone*</label>
+                            <input name = "tel" type = "tel" pattern = "[0-9]{10}" value = "<?= isset($_SESSION['tel']) ? $_SESSION['tel'] : ''; ?>" maxlength = "10" class = "form-control" id = "champTel" required />
                         </div>
-                        <div class="form-group text-center">
-                            <label for="champInfo">Informations complémentaires</label>
-                            <textarea name="info" class="form-control" id="champInfo" rows="3"
-                                      placeholder="Intérieur, Terrasse, poussette, etc..."></textarea>
+                        <div class = "form-group text-center">
+                            <label for = "champInfo">Informations complémentaires</label>
+                            <textarea name = "info" class = "form-control" id = "champInfo" rows = "3"
+                                      placeholder = "Intérieur, Terrasse, poussette, etc..."><?= isset($_SESSION['info']) ? $_SESSION['info'] : '';
+                                ?></textarea>
                         </div>
-                        
+
                         <div class="erreur mb-3 small text-center"><?= implode('<br/>', $tabErreur) ?></div>
                         <div class="text-center">
                             <input name="submit" type="submit" class="btn btn-primary mb-2" value="Réserver !"></button>
